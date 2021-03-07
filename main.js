f9d0ef61eefd860fd6343ed2d818f932e0eee383
Jonah Venglarcik <jonahvenglarcik@gmail.com>
2021-03-07 00:43:54 -0700

    Moved some style contents to styles.css

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    makeThemeButton('theme-link', 'theme-toggle');
+    makeThemeButton('theme', 'theme-toggle',
+                    'theme-dark.css', 'theme-light.css');
 });
 
-var makeThemeButton = function(themeLink, buttonClass) {
+var makeThemeButton = function(themeLink, buttonClass, darkFile, lightFile) {
     var buttons = document.getElementsByClassName(buttonClass);
     var theme = document.getElementById(themeLink)
 
     for (var i = 0; i < buttons.length; i++) {
         button = buttons[i];
         button.addEventListener('click', function() {
-            if (theme.getAttribute("href") == "styles-dark.css") {
-                theme.href = "styles-light.css";
+            if (theme.getAttribute("href") == darkFile) {
+                theme.href = lightFile;
             } else {
-                theme.href = "styles-dark.css";
+                theme.href = darkFile;
             }
         });
     }
