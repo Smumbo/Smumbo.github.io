d1a56c97577fc41c145994fbeebcaa9430643ea5
Jonah Venglarcik <jonahvenglarcik@gmail.com>
2021-03-08 15:55:27 -0700

    Added Roboto Condensed Bold Italic font

--- main.js
+++ main.js
@@ -7,12 +7,17 @@ var makeThemeButton = function(themeLink, buttonClass, darkFile, lightFile) {
     var buttons = document.getElementsByClassName(buttonClass);
     var theme = document.getElementById(themeLink)
 
+    // add listener to any/all theme toggle buttons
     for (var i = 0; i < buttons.length; i++) {
         button = buttons[i];
+
+        // add listener to current button
         button.addEventListener('click', function() {
             if (theme.getAttribute("href") == darkFile) {
+                // If current theme is dark, change to light
                 theme.href = lightFile;
             } else {
+                // If current theme is light, change to dark
                 theme.href = darkFile;
             }
         });
